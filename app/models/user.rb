4e83a53826b6897ccf3e88393705fde775312477
Jerry Chang <therealjerrycan@gmail.com>
2015-03-14 21:15:13 -0700

    added column for user's info hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
+ serialize :info 
  def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider 
       user.uid      = auth.uid
       user.name     = auth.info.name
+      user.info     = auth.info 
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save
     end
