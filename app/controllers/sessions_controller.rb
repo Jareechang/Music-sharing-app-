6bc2788d6ce8d18232008db50e84fcb4ee8d172d
Jerry Chang <therealjerrycan@gmail.com>
2015-05-08 15:05:01 -0700

    cleaned up code and added comments for clarity [Jerry Chang]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
 
+  # creates or finds User's successful OAuth Login 
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_url
   end
 
+  # Destroys the User session when logging out 
   def destroy
     session[:user_id] = nil
     redirect_to root_url
