88a4d289d463d554bf4c1083ae6cae53568c2905
Jerry Chang <therealjerrycan@gmail.com>
2015-05-10 16:18:09 -0700

    created a tracks factory for interacting with back-end API [Jerry Chang]

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,6 +1,24 @@
-angular.module('flapperNews', ['templates'])
+//This is the defining the module 
+angular.module('flapperNews', ['templates', 'ngResource'])
+
+// ************* Start of factory ***********************
+
+.factory('Tracks', ['$resource', function ($resource) {
+    return $resource('/tracks', {}, {
+        query: { method: 'GET', isArray: true }
+    })
+}])
+
+// ************* end of factory **************************
+
+
+// ************* start of main controller ***************
+
 .controller('MainCtrl', [
-'$scope',
-function($scope){
+'$scope', 'Tracks',
+function($scope, Tracks){
   $scope.test = 'Hello world!';
+  $scope.tracks = Tracks.query();
 }]);
+
+// ************* end of main controller ***************
