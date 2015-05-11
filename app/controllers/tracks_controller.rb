aeb27da4fd842ff54cb4bcef1950009d0acd94c8
Jerry Chang <therealjerrycan@gmail.com>
2015-05-10 18:04:18 -0700

    moved the soundcloud API into environment variables [Jerry Chang]

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,12 +1,11 @@
 class TracksController < ApplicationController
   def add_tracks
     # create a client object with your app credentials
-    client = Soundcloud.new(:client_id => '469a718d885e7cec05d02939174291c7')
+    client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_CLIENT_ID'])
 
     # find all sounds of buskers licensed under 'creative commons share alike'
     @tracks = client.get('/tracks', :q => params[:text], :limit => 20)
     
-
     @tracks.map! do |obj| 
       # Sends request to soundcloud API to gather all the Oembed info 
       # and gathers only the html widget for embedding into website 
