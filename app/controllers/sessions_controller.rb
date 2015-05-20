300102368466ffc3e88e897e1760c27694b5edc8
Jerry Chang <therealjerrycan@gmail.com>
2015-05-19 23:45:18 -0700

    testing deployed facebook friends query on heroku server [Jerry Chang]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,6 @@ class SessionsController < ApplicationController
 
   # creates or finds User's successful OAuth Login 
   def create
-    puts 'This is the env omniauth '
-    puts '  '   
-    puts env["omniauth.auth"]
-    puts ' '
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_url
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
+    if session[:user_id]
+      user = User.find(session[:user_id])
+      @test = facebook_friends(user.oauth_token)
+    end
+  end
+  
+  private 
+
+  def facebook_friends(token)
+    gab_id = '10153487298377454'
+    Koala.config.api_version = "v2.3"
+    @graph = Koala::Facebook::API.new(token , ENV['FACEBOOK_SECRET'])
+    friends = @graph.get_connections("me", "friends")
+    friends
   end
 end
